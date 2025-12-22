28bc1a442b4974bf4b68dcb04c474d2d3dfe4189
Yuji Kunitake <andrekuni123@gmail.com>
2025-12-22 00:29:04 -0300

    feat: create user base model

---
+++ app/models/user_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from app.core.database import BaseModel
+
+
+class UserModel(BaseModel):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    full_name = Column(String, nullable=True)
+
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
