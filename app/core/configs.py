3f903f0e6ae4d9bd596a851626719aefe1782a3e
Yuji Kunitake <andrekuni123@gmail.com>
2025-12-22 00:23:16 -0300

    feat: setup .env file and update .gitignore

--- app/core/configs.py
+++ app/core/configs.py
@@ -2,15 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = "Login System with FastAPI"
-    DATABASE_URL: str = "sqlite+aiosqlite:///./sql_app.db"
-    SECRET_KEY: str = "c6f025835e7a834859e51c6d79b7877eb3a20b631e855c02e30876d7c236d90d"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+    API_V1_STR: str
+    PROJECT_NAME: str
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(
         case_sensitive=True,
         env_file=".env",
+        env_file_encoding="utf-8",
     )
 
 
