58e3d873ef84a0d6aa8068cf7103ad57d500b7f1
Yuji Kunitake <andrekuni123@gmail.com>
2025-12-22 00:25:12 -0300

    fix: include ALGORITHM variable o configs

--- app/core/configs.py
+++ app/core/configs.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str
 
     model_config = SettingsConfigDict(
         case_sensitive=True,
